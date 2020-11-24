a8ba4e33cb1d044ebca081bc376a179058029196
Dongjin Kim <tobetter@gmail.com>
2020-11-25 03:43:28 +0900

    thermal: amlogic_thermal: Add hwmon support
    Expose Amlogic thermal as HWMON devices.
    
    	$ sensors
    	cpu_thermal-virtual-0
    	Adapter: Virtual device
    	temp1:        +32.2 C  (crit = +110.0 C)
    
    	ddr_thermal-virtual-0
    	Adapter: Virtual device
    	temp1:        +33.4 C  (crit = +110.0 C)
    
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>
    Change-Id: Icb1bf7cd7e4462f9923af5bcd72fa7c0c8e14cc9

--- drivers/thermal/amlogic_thermal.c
+++ drivers/thermal/amlogic_thermal.c
@@ -314,6 +314,9 @@ static int amlogic_thermal_probe(struct platform_device *pdev)
 	if (ret)
 		return ret;
 
+	if (devm_thermal_add_hwmon_sysfs(pdata->tzd))
+		dev_warn(&pdev->dev, "failed to add hwmon sysfs attributes\n");
+
 	ret = amlogic_thermal_enable(pdata);
 
 	return ret;
